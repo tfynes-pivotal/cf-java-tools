164e47e3e567e5e98d2c9b2c97cc1d06987e9662
Mark Fynes <tfynes@vmware.com>
2024-06-28 15:48:25 -0400

    .

--- deploy-demos.sh
+++ deploy-demos.sh
@@ -5,7 +5,10 @@ cf set-env tdemo JBP_CONFIG_OPEN_JDK_JRE "{\"jre\":{\"version\":\"21.+\"}}"
 cf start tdemo
 sleep 1
 curl https://tdemo.homelab.fynesy.com
+sleep 1
 ./cf-java-optimizer.sh tdemo
+sleep 1
+cf stop tdemo
 
 cf push customer-profile -p ./samples/Customer-Profile/target/customer-profile-0.0.1-SNAPSHOT.jar --no-start
 cf set-env customer-profile JBP_CONFIG_OPEN_JDK_JRE "{\"jre\":{\"version\":\"21.+\"}}"
@@ -13,5 +16,9 @@ cf bs customer-profile customer-database
 cf start customer-profile
 sleep 1
 curl https://customer-profile.homelab.fynesy.com/api/customer-profiles
-./experimental/cf-java-optimizer-app-with-binding.sh customer-profile
+curl https://customer-profile.homelab.fynesy.com/api/customer-profiles -X POST -d '{"firstName":"Mark","lastName":"Fynes","email":"a@b.com"}' -H "Content-Type: application/json"
 
+sleep 1
+./experimental/cf-java-optimizer-app-with-binding.sh customer-profile
+sleep 1
+cf stop customer-profile
